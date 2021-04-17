afdcd914357973a2f9af6e798c80f07cbce23366
marcoribera <marcoribera3@gmail.com>
2021-04-17 13:19:56 -0300

    Inseridas funções para extrair material sem skill

---
+++ Scripts/Items/Custom/fakeMinerio.cs
@@ -0,0 +1,142 @@
+using Server.Engines.Harvest;
+using Server.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Items
+{
+    public class fakeMinerio : Item
+    {
+        private int m_usosRestantes;
+
+        [Constructable]
+        public fakeMinerio()
+            : base(0x08E3)
+        {
+            Hue = 997;
+            m_usosRestantes = 500;
+            Movable = false;
+            Name = "Rocha Escurecida";
+        }
+
+        public fakeMinerio(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void OnDoubleClick(Mobile from)
+        {
+            if (!from.InRange(GetWorldLocation(), 1))
+            {
+                from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
+                return;
+            }
+            else if (from.BeginAction(typeof(InstrumentedAddonComponent)))
+            {
+                var picareta = from.Weapon is Pickaxe;
+                if (!picareta)
+                {
+                    from.SendMessage("Talvez você possa usar isto com uma picareta nas mãos");
+                    from.EndAction(typeof(InstrumentedAddonComponent));
+                    return;
+                }
+                from.Direction = from.GetDirectionTo(this.Location);
+                from.Animate(AnimationType.Attack, Utility.RandomList(Mining.System.OreAndStone.EffectActions));
+                from.PlaySound(Utility.RandomList(Mining.System.OreAndStone.EffectSounds));
+
+                if (!from.InRange(GetWorldLocation(), 1))
+                {
+                    from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
+                    from.EndAction(typeof(InstrumentedAddonComponent));
+                    return;
+                }
+
+                Timer.DelayCall(TimeSpan.FromSeconds(1), () =>
+                {
+                    from.Direction = from.GetDirectionTo(this.Location);
+                    from.Animate(AnimationType.Attack, Utility.RandomList(Mining.System.OreAndStone.EffectActions));
+                    from.PlaySound(Utility.RandomList(Mining.System.OreAndStone.EffectSounds));
+                
+
+                    if (!from.InRange(GetWorldLocation(), 1))
+                    {
+                        from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
+                        from.EndAction(typeof(InstrumentedAddonComponent));
+                        return;
+                    }
+
+                    Timer.DelayCall(TimeSpan.FromSeconds(1), () =>
+                    {
+                        from.Direction = from.GetDirectionTo(this.Location);
+                        from.Animate(AnimationType.Attack, Utility.RandomList(Mining.System.OreAndStone.EffectActions));
+                        from.PlaySound(Utility.RandomList(Mining.System.OreAndStone.EffectSounds));
+
+                        if (!from.InRange(GetWorldLocation(), 1))
+                        {
+                            from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
+                            from.EndAction(typeof(InstrumentedAddonComponent));
+                            return;
+                        }
+                        Timer.DelayCall(TimeSpan.FromSeconds(1), () =>
+                        {
+
+                            Item minerio = new IronOre(1);
+
+                            Item igualnabag = null;
+
+                            foreach (var i in from.Backpack.Items)
+                            {
+                                if (i.WillStack(from, minerio))
+                                {
+                                    igualnabag = i;
+                                    break;
+                                }
+                            }
+                            if (igualnabag != null)
+                            {
+                                igualnabag.StackWith(from, minerio);
+                            }
+                            else
+                            {
+                                from.Backpack.AddItem(minerio);
+                            }
+
+                            from.SendMessage("Pegou Ferro");
+
+                            m_usosRestantes--; //Marcknight: Desgasta com os usos
+
+                            if (m_usosRestantes < 1)
+                            {
+                                from.SendMessage("A pedra desintegrou!");
+                                this.Delete();
+                            }
+                            from.EndAction(typeof(InstrumentedAddonComponent));
+                            return;
+                        });
+                    });
+                });
+            }
+            else
+            {
+                from.SendLocalizedMessage(500119); // You must wait to perform another action
+            }
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)0); // version
+            writer.Write(m_usosRestantes); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+            m_usosRestantes = reader.ReadInt();
+        }
+    }
+}
