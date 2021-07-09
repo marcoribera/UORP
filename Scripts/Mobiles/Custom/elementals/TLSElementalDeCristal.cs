b37b88ebe25f4b7c3c5bc25e1b6089e3b79b46b1
lucasbqf <lucasbqf@hotmail.com>
2021-07-08 21:51:25 -0300

    Alterado nomes para padrao discutido com Pizza
    todos os Mobs foram re-nomedados com a sigla TSL no inicio.

---
+++ Scripts/Mobiles/Custom/elementals/TLSElementalDeCristal.cs
@@ -0,0 +1,93 @@
+using System;
+using Server.Items;
+
+namespace Server.Mobiles
+{
+    [CorpseName("corpo do Elemental de cristal")]
+    public class TLSElementalDeCristal : BaseCreature
+    {
+        [Constructable]
+        public TLSElementalDeCristal()
+            : base(AIType.AI_Mage, FightMode.Closest, 10, 1, 0.2, 0.4)
+        {
+            Name = "Elemental de cristal";
+            Body = 1576;
+            BaseSoundID = 278;
+
+            SetStr(136, 160);
+            SetDex(51, 65);
+            SetInt(86, 110);
+
+            SetHits(150);
+
+            SetDamage(10, 15);
+
+            SetDamageType(ResistanceType.Physical, 80);
+            SetDamageType(ResistanceType.Energy, 20);
+
+            SetResistance(ResistanceType.Physical, 50, 60);
+            SetResistance(ResistanceType.Fire, 40, 50);
+            SetResistance(ResistanceType.Cold, 40, 50);
+            SetResistance(ResistanceType.Poison, 100);
+            SetResistance(ResistanceType.Energy, 55, 70);
+
+            SetSkill(SkillName.PoderMagico, 70.1, 75.0);
+            SetSkill(SkillName.Arcanismo, 70.1, 75.0);
+            SetSkill(SkillName.ResistenciaMagica, 80.1, 90.0);
+            SetSkill(SkillName.Anatomia, 75.1, 85.0);
+            SetSkill(SkillName.Briga, 65.1, 75.0);
+
+            Fame = 6500;
+            Karma = -6500;
+
+            VirtualArmor = 54;
+
+            SetWeaponAbility(WeaponAbility.ParalyzingBlow);
+        }
+
+        public TLSElementalDeCristal(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override bool BleedImmune
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public override Poison PoisonImmune
+        {
+            get
+            {
+                return Poison.Lethal;
+            }
+        }
+        public override int TreasureMapLevel
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public override void GenerateLoot()
+        {
+            AddLoot(LootPack.Rich);
+            AddLoot(LootPack.Average);
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+        }
+    }
+}
---
+++ Scripts/Mobiles/Custom/goblins/TLSGoblinMachado2.cs
@@ -0,0 +1,128 @@
+using System;
+using Server.Items;
+
+namespace Server.Mobiles
+{
+    [CorpseName("corpo de um goblin elite")]
+    public class TLSGoblinMachado2 : BaseCreature
+    {
+        [Constructable]
+        public TLSGoblinMachado2()
+            : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
+        {
+            Name = "um goblin Elite";
+
+            Body = 1782;
+            Hue = 0;
+            BaseSoundID = 0x600;
+
+            SetStr(50, 70);
+            SetDex(40, 70);
+            SetInt(15, 20);
+
+            SetHits(100, 120);
+            SetStam(60, 80);
+            SetMana(30, 30);
+
+            SetDamage(15, 20);
+
+
+            SetDamageType(ResistanceType.Physical, 100);
+
+            SetResistance(ResistanceType.Physical, 20, 40);
+            SetResistance(ResistanceType.Fire, 20, 40);
+            SetResistance(ResistanceType.Cold, 20, 40);
+            SetResistance(ResistanceType.Poison, 20, 40);
+            SetResistance(ResistanceType.Energy, 20, 40);
+
+            SetSkill(SkillName.ResistenciaMagica, 20, 40);
+            SetSkill(SkillName.Anatomia, 40, 60);
+            SetSkill(SkillName.Atirar, 40, 60);
+            SetSkill(SkillName.Briga, 40, 60);
+
+            Fame = 1500;
+            Karma = -1500;
+
+            VirtualArmor = 28;
+
+            switch ( Utility.Random(20) )
+            {
+                case 0:
+                    PackItem(new Scimitar());
+                    break;
+                case 1:
+                    PackItem(new Katana());
+                    break;
+                case 2:
+                    PackItem(new WarMace());
+                    break;
+                case 3:
+                    PackItem(new WarHammer());
+                    break;
+                case 4:
+                    PackItem(new Kryss());
+                    break;
+                case 5:
+                    PackItem(new Pitchfork());
+                    break;
+            }
+
+            PackItem(new ThighBoots());
+
+            switch ( Utility.Random(3) )
+            {
+                case 0:
+                    PackItem(new Ribs());
+                    break;
+                case 1:
+                    PackItem(new Shaft());
+                    break;
+                case 2:
+                    PackItem(new Candle());
+                    break;
+            }
+
+            if (0.2 > Utility.RandomDouble())
+                PackItem(new BolaBall());
+        }
+
+        public TLSGoblinMachado2(Serial serial)
+            : base(serial)
+        {
+        }
+		
+		public override int GetAngerSound() { return 0x600; }
+        public override int GetIdleSound() { return 0x600; }
+        public override int GetAttackSound() { return 0x5FD; }
+        public override int GetHurtSound() { return 0x5FF; }
+        public override int GetDeathSound() { return 0x5FE; }
+
+        public override bool CanRummageCorpses { get { return true; } }
+        public override int TreasureMapLevel { get { return 1; } }
+        public override int Meat { get { return 1; } }
+        public override TribeType Tribe { get { return TribeType.GreenGoblin; } }
+
+        public override void GenerateLoot()
+        {
+            AddLoot(LootPack.Meager);
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)1);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+
+            if (version == 0)
+            {
+                Body = 723;
+                Hue = 1900;
+            }
+        }
+    }
+}
